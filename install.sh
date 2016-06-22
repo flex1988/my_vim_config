3c98c852d1cd55c9d48b34bd114541912560ff04
chenfei7 <chenfei7@bj-m-203687a.local>
2016-06-22 19:26:49 +0800

    add vundle

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp my_configs.vim ~/.vim_runtime
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
