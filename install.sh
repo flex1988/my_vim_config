8e9595642afa065d848b7cacfdbd26285a93ee2b
chenfei7 <chenfei7@staff.weibo.com>
2017-02-13 12:15:54 +0800

    add fish script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-git clone https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-cp my_configs.vim ~/.vim_runtime
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+#!/bin/bash
+
+sh install_fish.sh
+sh install_vim.sh
---
+++ install_fish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /etc/yum.repos.d/
+wget http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_7/shells:fish:release:2.repo
+yum install fish
